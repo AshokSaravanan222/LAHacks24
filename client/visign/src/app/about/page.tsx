f20ef8742b04b8663b95ee8e65242c9c7c38d883
Ashok Saravanan <ashko.saravanan@gmail.com>
2024-04-20 01:13:40 -0700

    added header

---
+++ client/visign/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const page = () => { 
+  return (
+    <div>
+    <Header />
+    <div>Hello World</div>
+    </div>
+  )
+}
+
+export default page
