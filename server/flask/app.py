5badd3515ba0a82a1e76c4105551412bc64c13c7
Ashok Saravanan <ashko.saravanan@gmail.com>
2024-04-19 22:40:07 -0700

    added server code

---
+++ server/flask/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', )
+def hello_world():
+    # take in the frames
+    # run the model
+    # return the result
+    return 'Hello, World!'
+
+
+if __name__ == "__main__":
+	app.run()
