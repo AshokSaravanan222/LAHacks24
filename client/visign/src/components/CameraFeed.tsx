e28fe9a20b1176659cfcc06998d93442fffac1ce
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2024-04-20 02:06:11 -0700

    working?

--- client/visign/src/components/CameraFeed.tsx
+++ client/visign/src/components/CameraFeed.tsx
@@ -15,8 +15,8 @@ const CameraFeed: React.FC = () => {
         );
         handLandmarkerRef.current = await HandLandmarker.createFromOptions(vision, {
             baseOptions: {
-                // modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
-                modelAssetPath: `models/hand_landmarker.task`,
+                modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+                // modelAssetPath: `models/hand_landmarker.task`,
                 delegate: "GPU"
             },
             runningMode: "IMAGE",
