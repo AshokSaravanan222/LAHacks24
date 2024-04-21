6338bfbd6742b57f333b2450a5d35482662b1a53
Geoffrey Lee <geoffrey0953@gmail.com>
2024-04-20 23:14:29 -0700

    sample about page

--- client/visign/src/app/about/page.tsx
+++ client/visign/src/app/about/page.tsx
@@ -1,13 +1,29 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
-const page = () => { 
+const AboutUsPage = () => {
   return (
     <div>
-    <Navbar />
-    <div>Hello World</div>
+      <Navbar />
+      <div style={{ margin: '15px', textAlign: 'center' , color: 'white'}}>
+        <h1>About Us</h1>
+        <p>Welcome to ViSign, a place where we serve to bridge the communication gaps between the deaf and hearing communities. .</p>
+        <section style={{ marginTop: '30px' }}>
+          <h2>Our Mission</h2>
+          <p>[Describe the mission of your organization, what you're passionate about, and what you aim to achieve.]</p>
+        </section>
+        <section>
+          <h2>Our Team</h2>
+          <p>Geoffrey, Wayne, Ashok, Vib</p>
+        </section>
+        <section>
+          <h2>Contact Us</h2>
+          <p>Feel free to reach out via email at EMAIL or follow us on our social media pages at ViSign</p>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+// Exporting the component for use in other parts of your application
+export default AboutUsPage;
